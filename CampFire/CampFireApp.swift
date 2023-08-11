3e3356eaa5d330835381754e71eddf1b54848873
Dedeti <andrenes2905@gmail.com>
2023-08-11 13:52:34 -0300

    Initial Commit

---
+++ CampFire/CampFireApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampFireApp.swift
+//  CampFire
+//
+//  Created by André Enes Pecci on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CampFireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

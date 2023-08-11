69645768aa3aaa8a76559b62517b947a43290532
Dedeti <andrenes2905@gmail.com>
2023-08-11 14:16:00 -0300

    Fonseca - Desenvolvimento dos textos e do símbolo

--- CampFire/CardView.swift
+++ CampFire/CardView.swift
@@ -10,11 +10,37 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         VStack {
+            
             HStack {
-                Text
+                Text("Harold Garcia")
             }
+            .padding(.vertical)
+            
+            HStack{
+                Text("Delivery Staff")
+                    .rotationEffect(.degrees(270))
+                    .font(.caption)
+
+                
+                Image(systemName: "command")
+                    .font(.largeTitle)
+//                    .padding(.horizontal)
+                Spacer()
+                    
+                Text("Delivery Staff")
+                    .rotationEffect(.degrees(90))
+                    .font(.caption)
+                
+            }
+            
+            Text("web: www.highway.com")
+                .font(.caption)
+            Text("tel: 130-704-5777")
+                .font(.caption)
+            
+            
         } .frame(width: 300, height: 170)
-        .background(.red)
+        .background(.gray)
     }
 }
 

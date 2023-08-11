a4bd90b706de82d5659d108a31b5ffa72155fc72
Dedeti <andrenes2905@gmail.com>
2023-08-11 16:43:26 -0300

    André resolveu tudo!!!!

--- CampFire/Cantor/CantorView.swift
+++ CampFire/Cantor/CantorView.swift
@@ -9,27 +9,39 @@ import SwiftUI
 
 struct CantorView: View {
     var body: some View {
-        HStack {
-            Image("paul")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 250, height: 350, alignment: .leading)
-                .padding(.leading, -20)
-               
-            Spacer()
-            VStack {
-                Text("Paul")
-                    .font(.title)
-                    .foregroundColor(.black)
-//                    .padding(.trailing, 70)
-                    .padding(.trailing, 50)
-                    Text(
+        VStack {
+            Image(systemName: "xmark.circle.fill")
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            HStack {
+                Image("paul")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 250, height: 350, alignment: .leading)
+                    .padding(.leading, -20)
+                
+                Spacer()
+                VStack(alignment: .leading) {
+                    
+                    
+                    
+                    Text("Paul")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .padding(.vertical)
+                    
+                    Text("John Roger Steph (born December 28, 1978,better known by his tage name John Legend, is an American singer, songwriter and actor.\n\nJohn Roger Steph (born December 28, 1978,better known by his tage name John Legend, is an American singer, songwriter and actor.")
+                        .font(.system(size:10))
+                        .foregroundColor(.gray)
+                        .padding(.trailing, 15)
+                    
+                    
+                    
+                }
                 
             }
-            
+            .frame(width: 400, height: 350)
+        .background(.white)
         }
-        .frame(width: 400, height: 350)
-        .background(.gray)
         
     }
 }
@@ -37,5 +49,7 @@ struct CantorView: View {
 struct CantorView_Previews: PreviewProvider {
     static var previews: some View {
         CantorView()
+            .previewLayout(.sizeThatFits)
+        //            .padding()
     }
 }
